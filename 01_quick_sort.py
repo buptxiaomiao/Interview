796f2e72a872bf2428e8e3725f3b1f2f745e66c6
wjh <buptwjh@outlook.com>
2018-08-30 20:41:45 +0800

    update 混沌

---
+++ 01_quick_sort.py
@@ -0,0 +1,10 @@
+#########################################################################
+# coding: utf-8
+#!/usr/bin/python
+# File Name: 01_quick_sort.py
+# Author: buptxiaomiao
+# mail: buptwjh@outlook.com
+# Created Time: 六  8/18 10:11:59 2018
+#########################################################################
+
+def quick
